87eff9fd321152140e6211711563d3cf3422145b
JPAN14 <32107181+JPAN14@users.noreply.github.com>
2017-09-19 14:32:37 -0500

    Add files via upload

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,25 @@
+import 
+{ 
+	Component, 
+	OnInit,
+	Input // <-- added this 
+} 
+from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+  @Input() name: string; // <-- added name property
+
+  constructor() 
+  { 
+	//this.name = 'Felipe'; // set the name	
+  }
+
+  ngOnInit() {
+  }
+
+}
